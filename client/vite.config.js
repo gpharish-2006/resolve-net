0fc8d562260a426ad65d26be8c06dfcf49e26a3e
lovelyharish06 <gpharish2006@gmail.com>
2026-05-20 16:42:03 +0530

    Frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+
   plugins: [react()],
-  base: '/resolve-net/', // <-- ADD THIS LINE
+
+  base:
+    process.env.NODE_ENV === "production-gh"
+      ? "/resolve-net/"
+      : "/"
+
 })
