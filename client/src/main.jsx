d99b8f7401881f386369a4496df51af2c93ed46f
lovelyharish06 <gpharish2006@gmail.com>
2026-05-27 19:10:51 +0530

    Final

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
--- server/server.mjs
+++ server/server.mjs
@@ -14,8 +14,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-    "http://localhost:5173", 
-    "https://resolve-net.vercel.app"
+      process.env.CLIENT_LOCAL_URL,
+      process.env.CLIENT_PROD_URL
   ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
